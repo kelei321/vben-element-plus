9b2f36217dde3003e1ffa98da2340a3deb14ac82
kelei321 <51449002+kelei321@users.noreply.github.com>
2026-07-23 10:44:46 +0800

    refactor: localize route module utilities

---
+++ src/router/route-modules.ts
@@ -0,0 +1,41 @@
+type RouteRecordName = string | symbol;
+
+interface RouteTreeLike {
+  children?: RouteTreeLike[];
+  name?: RouteRecordName;
+}
+
+interface RouteModule<Route> {
+  default?: Route[];
+}
+
+function mergeRouteModules<Route>(
+  routeModules: Record<string, unknown>,
+): Route[] {
+  const mergedRoutes: Route[] = [];
+
+  for (const routeModule of Object.values(routeModules)) {
+    const moduleRoutes = (routeModule as RouteModule<Route>)?.default ?? [];
+    mergedRoutes.push(...moduleRoutes);
+  }
+
+  return mergedRoutes;
+}
+
+function collectRouteNames(routes: RouteTreeLike[]): RouteRecordName[] {
+  const routeNames: RouteRecordName[] = [];
+
+  const visitRoute = (route: RouteTreeLike) => {
+    if (route.name) {
+      routeNames.push(route.name);
+    }
+
+    route.children?.forEach((childRoute) => visitRoute(childRoute));
+  };
+
+  routes.forEach((route) => visitRoute(route));
+
+  return routeNames;
+}
+
+export { collectRouteNames, mergeRouteModules };
