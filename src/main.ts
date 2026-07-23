7107a450ed0ea5952900e789a06962d85ac54f54
kelei321 <51449002+kelei321@users.noreply.github.com>
2026-07-23 09:13:16 +0800

    refactor: use localized root application bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,21 @@
-import '../apps/web-ele/src/main';
+import { initPreferences } from '@vben/preferences';
+
+import { bootstrap } from '../apps/web-ele/src/bootstrap';
+import { overridesPreferences } from '../apps/web-ele/src/preferences';
+import { unmountGlobalLoading } from './shared/utils/loading';
+
+async function initApplication() {
+  const env = import.meta.env.PROD ? 'prod' : 'dev';
+  const appVersion = import.meta.env.VITE_APP_VERSION;
+  const namespace = `${import.meta.env.VITE_APP_NAMESPACE}-${appVersion}-${env}`;
+
+  await initPreferences({
+    namespace,
+    overrides: overridesPreferences,
+  });
+
+  await bootstrap(namespace);
+  unmountGlobalLoading();
+}
+
+void initApplication();
